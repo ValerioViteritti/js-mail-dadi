ea108f357ec939d2c0c4e047f7a586c50daa71d8
ValerioViteritti <vviteritti@gmail.com>
2024-06-09 11:34:54 +0200

    0.1 Creati i 2 esercizi, funzionanti in console.

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -1,35 +1,34 @@
+// Gioco dei dadi :dado_da_gioco:
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
-// Creo dei div
-let square = document.createElement('div');
-square.className = ('d-flex flex-wrap justify-content-center align-items-center gap-4')
+// creiamo i 2 dadi
 
-let varianti = '';
+let dicePlayer = [1, 2, 3, 4, 5, 6]
+let dicePc = [1, 2, 3, 4, 5, 6]
 
-// Ciclo
-for (let i = 1; i <= 100; i++){
-    
-    // creiamo dei box con i numeri progressivi all'interno
-    // const element = `<div class="box">${i}</div>`;
+// creo il generatore dei lanci
 
-        if(i % 3 === 0 && i % 5 === 0){
-            varianti += `<div class="xxx fizzbuzz">${'FIZZBUZZ'}</div>`;
-            console.log("FIZZBUZZ");
-  
-        } else if(i % 3 === 0){
-            varianti += `<div class="xxx fizz">${'FIZZ'}</div>`;
-            console.log("FIZZ");
+let randomPlayer = Math.floor(Math.random()*6);
 
-        } else if(i % 5 === 0){
-            varianti += `<div class="xxx buzz">${'BUZZ'}</div>`;
-            console.log("BUZZ");
+console.log(randomPlayer);
 
-        } else{
-            varianti += `<div class="xxx">${i + 0}</div>`;
-            console.log(i);
-        }   
+let randomPc = Math.floor(Math.random()*6);
+
+console.log(randomPc);
+
+// Chi vince
+
+if(randomPlayer > randomPc){
+    randomPlayer = "Vince Player"
+
+    console.log(randomPlayer);
+} else if(randomPlayer < randomPc){
+    randomPc = "Vince Pc"
+
+    console.log(randomPc);
+} else {
+    Pari = "Pari"
+
+    console.log(Pari);
 }
-
-square.innerHTML = varianti;
-// Collegamento all'html
-const container = document.querySelector('.container')
-container.append(square)
+   
